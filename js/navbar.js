d7f0821f8c1389aa9e15afa2ec3d9bafc8206af2
qJFI <ymkhhk2005@gmail.com>
2024-05-18 04:23:01 +0300

    animating block simulator done

---
+++ js/navbar.js
@@ -0,0 +1,15 @@
+document.write(`<nav>
+<a href="index.html"><img width="50px" src="./assets/images/M logo.png " alt="Header">&nbsp;&nbsp;&nbsp;</a>
+  <a href="index.html"> OS MSA</a>
+  <div class="nav-links" id="navLinks">
+      <i class="fas fa-window-close" onclick="hideMenu()"></i>
+      <ul>
+          <li><a href="index.html">HOME</a></li>
+          <li><a href="docs.html">DOCS</a></li>
+          <li><a href="./ganttcharts.html">SIMULATOR</a></li>
+          
+          <li><a href="./visualization2.html">SIMULATOR2</a></li>
+      </ul>
+  </div>
+  <i class="fas fa-bars" onclick="showMenu()"></i>
+</nav>`)
